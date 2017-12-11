375dc2a6995def1b758d0c3f3266612060dccd96
Unknown <edwynluis@yahoo.com.br>
2017-12-11 17:27:50 +0100

    activating Collectible on test map

--- Assets/Scripts/Test/CollectibleTest.cs
+++ Assets/Scripts/Test/CollectibleTest.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CollectibleTest : MonoBehaviour {
 
+	public CollectibleController collectible;
+	public CollectibleElement.CollectibleType collectibleType;
+
 	// Use this for initialization
 	void Start () {
-		
+		collectible.Collectible = new CollectibleElement ( this.collectibleType, new Vector2(0,0) );;
 	}
 	
 	// Update is called once per frame
